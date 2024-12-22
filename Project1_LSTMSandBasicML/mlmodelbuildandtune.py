96f9dab405c0c932856e553d3d7e5da85f1ec5da
Matthewos Mesfin <matthewosmesfin@Matthewoss-MacBook-Pro.local>
2024-12-22 12:39:22 -0500

    Hypertuning

---
+++ Project1_LSTMSandBasicML/mlmodelbuildandtune.py
@@ -0,0 +1,9 @@
+#create functions for the following:
+
+#reading input data
+#splitting it into training and testing set
+#running ml model
+#hypertuning on training set
+#outputting
+
+#that way we don't have to keep rewriting it everytime
